6f1c50986ba2e5376975f3d1e5eabcc898618b74
Austin Harris <austinharris@austins-mbp.3520.dhcp.asu.edu>
2023-03-23 14:51:37 -0700

    functionality of task 3 now works; this version includs all my prints to test it. i still have yet to make the first set be in the order that the professor wants, so thats next

--- project2.cc
+++ project2.cc
@@ -11,6 +11,8 @@
 #include <vector>
 #include <deque>
 #include <unordered_set>
+#include <stack>
+#include <unordered_map>
 
 using namespace std;
 
@@ -32,6 +34,10 @@ bool validRHS(LexicalAnalyzer *);
 bool validIDList(LexicalAnalyzer *);
 unordered_set<string> findUnorderedNonterminals();
 pair<deque<string>, deque<string>> findOrdered_TerminalsAndNonTerminals();
+bool isNonterminal(string);
+bool goesToEpsilon(string);
+unordered_map<string, deque<string>> CalculateFirstSets();
+void printFirstSets(unordered_map<string, deque<string>>);
 // recursive procedures to check if the grammar for an input grammar is being followed
 
 // read grammar
@@ -216,20 +222,20 @@ pair<deque<string>, deque<string>> findOrdered_TerminalsAndNonTerminals()
         }
 
         for (int i = 0; i < iterator->rhs.size(); i++)
-        { 
+        {
 
-            if (nonterminals_unordered.count(iterator->rhs[i]) == 1) //if its a nonterminal
-            { 
-                if (inserted_nonterminals.insert(iterator->rhs[i]).second) //and hasnt been inserted into our queue yet
+            if (nonterminals_unordered.count(iterator->rhs[i]) == 1) // if its a nonterminal
+            {
+                if (inserted_nonterminals.insert(iterator->rhs[i]).second) // and hasnt been inserted into our queue yet
                 {
-                    nonterminals.push_back(iterator->rhs[i]); 
+                    nonterminals.push_back(iterator->rhs[i]);
                 }
             }
             else
             {
-                if (inserted_terminals.insert(iterator->rhs[i]).second) //if its a terminal
+                if (inserted_terminals.insert(iterator->rhs[i]).second) // if its a terminal
                 {
-                    terminals.push_back(iterator->rhs[i]); 
+                    terminals.push_back(iterator->rhs[i]);
                 }
             }
         }
@@ -249,9 +255,186 @@ void RemoveUselessSymbols()
 }
 
 // Task 3
-void CalculateFirstSets()
+
+void printFirstSets(unordered_map<string, deque<string>> first_sets)
 {
-    cout << "3\n";
+
+    //add epsilons to first set and order it the way bazzi wants it to be, then print
+
+
+    //printing starts here
+    deque<string> nonterminals = findOrdered_TerminalsAndNonTerminals().second;
+
+    for (int k = 0; k < nonterminals.size(); k++)
+    {
+        deque<string> first_set = first_sets[nonterminals[k]];
+        cout << nonterminals[k] << ": ";
+        // cout << first_set.size();
+        for (int i = 0; i < first_set.size(); i++)
+        {
+            cout << first_set[i] << " ";
+        }
+        cout << endl;
+    }
+    // cout << "this: " << first_sets[nonterminals[1]].front() << endl;
+}
+
+unordered_map<string, deque<string>> CalculateFirstSets() // gonna fix order at the end cuz the order thing is fucking stupid
+{
+
+    deque<string> rules = findOrdered_TerminalsAndNonTerminals().second;
+
+    deque<string> update_first_sets;
+    unordered_set<string> update_first_sets_inserted;
+
+    unordered_map<string, deque<string>> first_sets;
+    unordered_map<string, unordered_set<string>> first_sets_inserted;
+    
+    /*struct rule *iterator = head;
+    while(iterator != NULL){
+        cout << "testing rule: " << iterator->lhs << endl;
+
+        if(goesToEpsilon(iterator->lhs)){
+            cout << iterator->lhs << "goes to epsilon" << endl;
+            if(first_sets_inserted[iterator->lhs].insert("#").second){
+                cout << "inserted epsilon for " << iterator->lhs << endl;
+                first_sets[iterator->lhs].push_front("#");
+            }
+        }
+
+        iterator = iterator->next;
+    }*/
+
+    while (!rules.empty()) // can skip over the eles that already have entries in the map
+    {
+        string curr_rule = rules.front();
+        update_first_sets.push_back(curr_rule);
+        update_first_sets_inserted.insert(curr_rule);
+        rules.pop_front();
+
+        while (!update_first_sets.empty())
+        {
+            cout << "rule: " << curr_rule << " with the queue of its first set eles:";
+            for (int q = 0; q < update_first_sets.size(); q++)
+            {
+                cout << update_first_sets[q] << " ";
+            }
+            cout << endl;
+            struct rule *iterator = head;
+
+            if(goesToEpsilon(update_first_sets.front())){
+                if(first_sets_inserted[curr_rule].insert("#").second){
+                    first_sets[curr_rule].push_front("#");
+                }
+            }
+            while (iterator != NULL)
+            { // find all terms and nonterms for rule at front of queue
+
+                if (iterator->lhs == update_first_sets.front())
+                { // the rule at front of queue
+
+                    if (iterator->rhs.size() == 0)
+                    {
+                    }
+                    else
+                    {
+                        cout << "found a rule that matches first node in our queue: " << iterator->lhs << endl;
+                        if (!isNonterminal(iterator->rhs[0]))
+                        {                                                                       // if its first element is a terminal
+                            if (first_sets_inserted[curr_rule].insert(iterator->rhs[0]).second) // check if we've already added it
+                                first_sets[curr_rule].push_back(iterator->rhs[0]);              // then add if not
+                        }
+                        else
+                        { // else if its a nonterminal, we need to check if we can go past it
+
+                            if (goesToEpsilon(iterator->rhs[0]))
+                            {
+                                if (update_first_sets_inserted.insert(iterator->rhs[0]).second)
+                                {
+                                    update_first_sets.push_back(iterator->rhs[0]);
+                                }
+
+                                for (int i = 1; i < iterator->rhs.size(); i++)
+                                {
+                                    if (isNonterminal(iterator->rhs[i]))
+                                    {
+                                        if (goesToEpsilon(iterator->rhs[i]))
+                                        {
+                                            if (update_first_sets_inserted.insert(iterator->rhs[i]).second)
+                                            {
+                                                update_first_sets.push_back(iterator->rhs[i]);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if (update_first_sets_inserted.insert(iterator->rhs[i]).second)
+                                            {
+                                                update_first_sets.push_back(iterator->rhs[i]);
+                                            }
+                                            break;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        if (first_sets_inserted[curr_rule].insert(iterator->rhs[i]).second)
+                                        {
+                                            first_sets[curr_rule].push_back(iterator->rhs[i]);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+
+                            else if (update_first_sets_inserted.insert(iterator->rhs[0]).second)
+                            {
+                                update_first_sets.push_back(iterator->rhs[0]);
+                            }
+                        }
+                    }
+                }
+
+                iterator = iterator->next;
+            }
+            update_first_sets.pop_front();
+        }
+        update_first_sets_inserted.clear();
+        // each unique ule has its vector of first terms
+        // for each rule
+        // if first rhs ele is term, add it to rule's first set and break
+        // could keep your own stack frame with a stack of vectors you need to go back and update
+        // else, while rhs ele is a nonterminal
+        //        go find the rule(s) for that nonterminal, and if all rules for this nonterm dont have first rhs as term
+    }
+    return first_sets;
+}
+
+bool goesToEpsilon(string rule)
+{
+
+    struct rule *iterator = head;
+    while (iterator != NULL)
+    {
+
+        if (iterator->lhs == rule)
+        {
+            if (iterator->rhs.size() == 0)
+            {
+                return true;
+            }
+        }
+        iterator = iterator->next;
+    }
+    return false;
+}
+
+bool isNonterminal(string element)
+{
+    unordered_set<string> nonterminals_unordered = findUnorderedNonterminals();
+    if (nonterminals_unordered.insert(element).second)
+    {
+        return false;
+    }
+    return true;
 }
 
 // Task 4
@@ -298,7 +481,7 @@ int main(int argc, char *argv[])
         break;
 
     case 3:
-        CalculateFirstSets();
+        printFirstSets(CalculateFirstSets());
         break;
 
     case 4:
